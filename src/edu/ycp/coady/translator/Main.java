a1ab16f6c00cd6374978e32cc6bc009cf0423427
Josh Coady <droidmaker42@gmail.com>
2015-01-09 16:48:20 -0500

    implemented minimal working server

--- src/edu/ycp/coady/translator/Main.java
+++ src/edu/ycp/coady/translator/Main.java
@@ -3,13 +3,27 @@
  */
 package edu.ycp.coady.translator;
 
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.webapp.WebAppContext;
+
 /**
  * @author jcoady9
  *
  */
 public class Main {
 	
-	public static void main(){
+	public static void main(String[] args) throws Exception{
+		Server server = new Server(8082);
 		
+		WebAppContext webContext = new WebAppContext();
+		webContext.setContextPath("/");
+		webContext.setWar("./war");
+		webContext.setResourceBase("./war/page/");
+		webContext.setWelcomeFiles(new String[] {"independentStudy.html"});
+		
+		server.setHandler(webContext);
+		
+		server.start();
+		server.join();
 	}
 }
