42729bdbab3f71a3925dab8ebb11a5214fe8d92b
jcoady9 <jcoady@ycp.edu>
2015-01-31 19:08:50 -0500

    started writing unit tests for Translator class

---
+++ tests/edu/ycp/coady/translator/models/TranslatorTest.java
@@ -0,0 +1,36 @@
+package edu.ycp.coady.translator.models;
+
+import static org.junit.Assert.*;
+
+public class TranslatorTest {
+
+    @org.junit.Before
+    public void setUp() throws Exception {
+        Translator translator = new Translator("one", "two");
+    }
+
+    @org.junit.Test
+    public void testSetSourceLanguage() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testGetSourceLanguage() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testSetTargetLanguage() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testGetTargetLanguage() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testTranslate() throws Exception {
+
+    }
+}
