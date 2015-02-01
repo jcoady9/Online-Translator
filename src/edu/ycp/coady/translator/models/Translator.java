8d1cce89bbb4ab05c3554358670e1314caec263d
jcoady9 <jcoady@ycp.edu>
2015-01-31 19:04:51 -0500

    made Translator class

---
+++ src/edu/ycp/coady/translator/models/Translator.java
@@ -0,0 +1,40 @@
+package edu.ycp.coady.translator.models;
+
+/**
+ * Translates a string of text from its source language to the targeted language it is desired in
+ */
+public class Translator {
+
+    private String sourceLanguage, targetLanguage;
+
+    //constructor
+    public Translator(String sourceLang, String targetLang){
+        sourceLanguage = sourceLang;
+        targetLanguage = targetLang;
+    }
+
+    //setter for source language
+    void setSourceLanguage(String sourceLang){
+        sourceLanguage = sourceLang;
+    }
+
+    //getter for source language
+    String getSourceLanguage(){
+        return sourceLanguage;
+    }
+
+    //setter for target language
+    void setTargetLanguage(String targetLang){
+        targetLanguage = targetLang;
+    }
+
+    //getter for target language
+    String getTargetLanguage(){
+        return targetLanguage;
+    }
+
+    //translates text from the source language to the target language
+    String translate(String text){
+        return text;
+    }
+}
--- src/edu/ycp/coady/translator/servlet/TranslatorServlet.java
+++ src/edu/ycp/coady/translator/servlet/TranslatorServlet.java
@@ -15,7 +15,7 @@ public class TranslatorServlet extends HttpServlet {
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         resp.setContentType("text/html");
         resp.setStatus(HttpServletResponse.SC_OK);
-        resp.getWriter().println("<h1>This is your conscious speaking....</h1>");
+        resp.getWriter().println("This is your conscious speaking....");
 	}
 	
 }
