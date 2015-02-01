414dd98c48b176de07a487ba935de72eb1a51d93
jcoady9 <jcoady@ycp.edu>
2015-01-31 19:05:06 -0500

    made Database interface

---
+++ src/edu/ycp/coady/translator/models/Database.java
@@ -0,0 +1,8 @@
+package edu.ycp.coady.translator.models;
+
+/**
+ * A simple interface for creating different implementations of database management systems (SQL, ORACLE, DERBY, and all that other crap) as necessary
+ */
+public interface Database {
+    //move along, nothing to see here
+}
