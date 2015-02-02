e98184ba0f4cd1bbb25a92f877f5f8d340056981
Josh Coady <droidmaker42@gmail.com>
2015-02-02 18:14:59 -0500

    added check for receiving text and language codes

--- src/edu/ycp/coady/translator/servlet/TranslatorServlet.java
+++ src/edu/ycp/coady/translator/servlet/TranslatorServlet.java
@@ -17,10 +17,22 @@ public class TranslatorServlet extends HttpServlet {
         String text = (String)req.getParameter("text");
         String source_lang = (String)req.getParameter("source_lang");
         String target_lang = (String)req.getParameter("target_lang");
-        Translator translator = new Translator(source_lang, target_lang);
-        resp.setContentType("text/html");
-        resp.setStatus(HttpServletResponse.SC_OK);
-        resp.getWriter().println(translator.translate(text + " : " + translator.getSourceLanguage() + " : " + translator.getTargetLanguage()));
+
+        //send a bad request response if the text or language codes are not sent in the request
+        if(text.equals("") || source_lang.equals("") || target_lang.equals("")){
+            resp.setContentType("text/html");
+            resp.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+            resp.getWriter().println("Please check that you have entered text and selected the source and target languages.");
+        }else if(target_lang.equals(source_lang)){
+            resp.setContentType("text/html");
+            resp.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+            resp.getWriter().println("Please check that the source and target languages are not the same.");
+        }else{
+            Translator translator = new Translator(source_lang, target_lang);
+            resp.setContentType("text/html");
+            resp.setStatus(HttpServletResponse.SC_OK);
+            resp.getWriter().println(translator.translate(text));
+        }
 	}
 	
 }
