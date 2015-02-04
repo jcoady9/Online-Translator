222f522894aedb595bd09aad83ddd9a96759020c
jcoady9 <jcoady@ycp.edu>
2015-02-04 17:34:30 -0500

    started writing DictionaryDB getData() method

--- src/edu/ycp/coady/translator/models/DictionaryDB.java
+++ src/edu/ycp/coady/translator/models/DictionaryDB.java
@@ -1,13 +1,50 @@
 package edu.ycp.coady.translator.models;
 
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpStatus;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.protocol.HttpClientContext;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+
+import java.io.IOException;
+
+
 /**
- * An implementation of Database to access a English-German database on a separate server.
+ * An implementation of Database to access a English-German database on a separate server powered by JDictd.
  */
 public class DictionaryDB implements Database {
 
-    private final String url = "";  //url of the dictionary server;
+    private final String url = "http://localhost:2628";  //url of the dictionary server;
 
     public String getData(String query){
+
+        String params = "";
+
+        CloseableHttpClient client = HttpClients.custom().build();
+        HttpGet httpGet = new HttpGet(url + params);
+        HttpClientContext context = HttpClientContext.create();
+
+        CloseableHttpResponse resp = null;
+        try {
+            resp = client.execute(httpGet, context);
+            if(resp.getStatusLine().getStatusCode() == HttpStatus.SC_OK){
+                HttpEntity entity = resp.getEntity();
+                return entity.getContent().toString();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }finally{
+            try {
+                resp.close();
+                client.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }
 
         return "";
     }
