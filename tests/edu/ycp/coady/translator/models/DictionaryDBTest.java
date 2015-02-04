b78780623502e5d93ede9829d238e6f78831268d
jcoady9 <jcoady@ycp.edu>
2015-02-04 17:33:59 -0500

    created DictionaryDBTest

---
+++ tests/edu/ycp/coady/translator/models/DictionaryDBTest.java
@@ -0,0 +1,25 @@
+package edu.ycp.coady.translator.models;
+
+import junit.framework.TestCase;
+
+public class DictionaryDBTest extends TestCase {
+
+    DictionaryDB dictDB;
+
+    public void setUp() throws Exception {
+        super.setUp();
+        dictDB = new DictionaryDB();
+    }
+
+    public void testGetData() throws Exception {
+
+    }
+
+    public void testDbFileExists() throws Exception {
+        assert(dictDB.dbFileExists() == false);
+    }
+
+    public void testIsActive() throws Exception {
+        assert(dictDB.isActive() == false);
+    }
+}
