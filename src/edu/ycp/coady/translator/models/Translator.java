ce8f8df43278eb88081054e04b9fb54980eb1264
jcoady9 <jcoady@ycp.edu>
2015-02-06 16:10:44 -0500

    replaced independent variables in Translator w/  TranslatorComponents model

--- src/edu/ycp/coady/translator/models/Translator.java
+++ src/edu/ycp/coady/translator/models/Translator.java
@@ -4,33 +4,32 @@ package edu.ycp.coady.translator.models;
  * Translates a string of text from its source language to the targeted language it is desired in
  */
 public class Translator {
-
-    private String sourceLanguage, targetLanguage;
+    
+    private TranslationComponents components;
 
     //constructor
     public Translator(String sourceLang, String targetLang){
-        sourceLanguage = sourceLang;
-        targetLanguage = targetLang;
+        components = new TranslationComponents(sourceLang, targetLang);
     }
 
     //setter for source language
     public void setSourceLanguage(String sourceLang){
-        sourceLanguage = sourceLang;
+        components.setSourceLanguage(sourceLang);
     }
 
     //getter for source language
     public String getSourceLanguage(){
-        return sourceLanguage;
+        return components.getSourceLanguage();
     }
 
     //setter for target language
     public void setTargetLanguage(String targetLang){
-        targetLanguage = targetLang;
+        components.setTargetLanguage(targetLang);
     }
 
     //getter for target language
     public String getTargetLanguage(){
-        return targetLanguage;
+        return components.getTargetLanguage();
     }
 
     //translates text from the source language to the target language
